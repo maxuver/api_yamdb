00591fcefdbd79081d5cb1569f11b9e3c36ae0ef
AndreyAlexPy <pu5hcar@yandex.ru>
2023-04-26 22:05:17 +0800

    create models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+
+class Titles(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    rating = models.IntegerField()
+    description = models.TextField()
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        related_name='genres',
+        blank=True,
+        null=True)
+    category = models.ForeignKey(
+        Category,
+        unique=True,
+        on_delete=models.SET_NULL,
+        related_name='categories',
+        blank=True,
+        null=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'category'], name='unique_name_category'
+            ),
+        ]
