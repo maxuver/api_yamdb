c315e60be6deba16c34fb43d1f3f81825b4d7e39
Kseniia Baryshnikova <ksunny2000@yandex.ru>
2023-04-26 19:34:16 +0800

    fix api urls, delete authentication part

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -8,7 +8,5 @@ v1_router = DefaultRouter()
 v1_router.register(...)
 
 urlpatterns = [
-    path('v1/', include('djoser.urls')),
-    path('v1/', include('djoser.urls.jwt')),
     path('v1/', include(v1_router.urls)),
 ]
