9a778861f725f0677028df5a38e287481d9a23be
Maxim Patsyuk <maxuverv@gmail.com>
2023-04-28 19:51:34 +0400

    System of registration and authentication

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,26 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 
-from .views import ...
+from api.views import (CategoryViewSet, GenreViewSet, TitleViewSet,
+                       ReviewViewSet, CommentViewSet)
+from api.views import (UsersViewSet, user_create_view,
+                       user_jwt_token_create_view)
 
-v1_router = DefaultRouter()
+router_version_1 = routers.DefaultRouter()
+router_version_1.register('users', UsersViewSet, basename='users')
+router_version_1.register('categories', CategoryViewSet,
+                          basename='categories')
+router_version_1.register('genres', GenreViewSet, basename='genres')
+router_version_1.register('titles', TitleViewSet, basename='titles')
+router_version_1.register(r'titles/(?P<title_id>\d+)/reviews',
+                          ReviewViewSet, basename='reviews')
+router_version_1.register(r'titles/(?P<title_id>\d+)/reviews'
+                          r'/(?P<review_id>\d+)/comments',
+                          CommentViewSet, basename='comments')
 
-v1_router.register(...)
 
 urlpatterns = [
-    path('v1/', include(v1_router.urls)),
+    path('v1/', include(router_version_1.urls)),
+    path('v1/auth/signup/', user_create_view),
+    path('v1/auth/token/', user_jwt_token_create_view)
 ]
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'Секретный ключ'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug tFurned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
