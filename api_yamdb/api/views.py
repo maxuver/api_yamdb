dd100271ae8c8d1933527a77d7b0b37ea40f812c
Maxim Patsyuk <maxuverv@gmail.com>
2023-04-29 22:54:19 +0400

    Remove some structure

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,7 +3,6 @@ from http import HTTPStatus
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
-from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, permissions, filters
