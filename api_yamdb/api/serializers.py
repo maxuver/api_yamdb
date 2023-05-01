d81a3c4bf72114753b311f21f5e32d63a83c8b41
Maxim Patsyuk <maxuverv@gmail.com>
2023-05-01 23:49:09 +0400

    Resolve issues with pytest

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,9 +1,19 @@
-from django.contrib.auth import get_user_model
-from rest_framework import serializers
+from rest_framework import serializers, validators
+
+from reviews.models import Title, Genre, Category, Review
 from users.models import User
+from users.validators import validate_username
+from rest_framework.validators import UniqueValidator
 
 
 class UsersSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        required=True,
+        max_length=150,
+        validators=[validate_username,
+                    UniqueValidator(queryset=User.objects.all())]
+    )
+
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name',
@@ -16,7 +26,83 @@ class CreateUserSerializer(serializers.ModelSerializer):
         fields = ('username', 'email')
         model = User
 
+    def validate(self, attrs):
+        if len(attrs['username']) > 150:
+            raise serializers.ValidationError(
+                'Длина имени пользователя не должна превышать 150 символов')
+        return attrs
+
 
 class UserJWTTokenCreateSerializer(serializers.Serializer):
+    username = serializers.CharField(
+        required=True,
+        max_length=150,
+        validators=[validate_username, ]
+    )
     confirmation_code = serializers.CharField(required=True)
-    username = serializers.CharField(required=True)
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ('name', 'slug')
+        model = Genre
+        lookup_field = 'slug'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ('name', 'slug')
+        model = Category
+        lookup_field = 'slug'
+
+
+class TitleReadSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer(many=True)
+    category = CategorySerializer()
+
+    class Meta:
+        fields = '__all__'
+        model = Title
+
+
+class TitleWriteSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(
+        slug_field='slug', queryset=Category.objects.all())
+    genre = serializers.SlugRelatedField(
+        many=True, slug_field='slug', queryset=Genre.objects.all())
+
+    class Meta:
+        fields = '__all__'
+        model = Title
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(default=serializers.
+                                          CurrentUserDefault(),
+                                          slug_field='username',
+                                          read_only=True)
+
+    class Meta:
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
+        model = Review
+
+    def validate(self, attrs):
+        request = self.context.get('request')
+        if request.method == 'POST':
+            review = Review.objects.filter(
+                author=request.user,
+                title=self.context.get('view').kwargs.get('title_id')
+            )
+            if review:
+                raise serializers.ValidationError('Отзыв уже оставлен!')
+        return attrs
+
+
+class ReviewListSerializer(serializers.ModelSerializer):
+    results = ReviewSerializer(many=True)
+
+    class Meta:
+        fields = ('count', 'next', 'previous', 'results')
+        model = Review
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,14 +1,85 @@
 from django.db import models
-
 from .validators import validate_actual_year
+
+from users.models import User
+
+
+CHOICES = [(i, i) for i in range(1, 11)]
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Title(models.Model):
-    name = models.CharField(
-        max_length=256,
-        verbose_name='Название'
+    name = models.CharField(max_length=256, verbose_name='Название')
+    year = models.IntegerField(
+        verbose_name='Год создания',
+        validators=[validate_actual_year])
+    rating = models.IntegerField(
+        verbose_name='Рейтинг',
+        default=5)
+    description = models.TextField(verbose_name='Описание')
+    genre = models.ManyToManyField(Genre, through='GenreTitle')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        blank=True,
+        null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'category'], name='unique_name_category'
+            ),
+        ]
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} {self.genre}'
+
+
+class Review(models.Model):
+    text = models.TextField()
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='reviews')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews')
+    score = models.IntegerField(
+        choices=CHOICES,
+        default=10
     )
-    year = models.PositiveSmallIntegerField(
-        verbose_name='Год выпуска',
-        validators=[validate_actual_year]
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True
     )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'author'],
+                name='title_author_unique_match'
+            ),
+        ]
+
+    def __str__(self):
+        return self.text
